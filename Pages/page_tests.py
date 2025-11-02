89782c361bb6a1e4b7cbb997c720b5a12f469655
CJ438837 <cedricjadot@msn.com>
2025-11-02 19:52:45 +0100

    Update page_tests.py

--- Pages/page_tests.py
+++ Pages/page_tests.py
@@ -46,7 +46,7 @@ def app():
 
     lancer_tests = st.button("🧠 Lancer les tests interactifs")
 
-        st.write("### 🧩 Aperçu du `types_df` avant lancement des tests")
+    st.write("### 🧩 Aperçu du `types_df` avant lancement des tests")
     st.dataframe(types_df)
 
     st.write("Colonnes détectées :", types_df.columns.tolist())
@@ -56,4 +56,5 @@ def app():
         with st.spinner("Analyse en cours... ⏳"):
             propose_tests_interactif_streamlit(types_df, distribution_df, df, mots_cles)
         st.success("✅ Les tests interactifs ont été exécutés avec succès.")
+
 
