0c617c51ff3dfb5893c8b69e833f89ebd02953c5
CJ438837 <cedricjadot@msn.com>
2025-11-02 16:51:28 +0100

    Update 4_distribution.py

---
+++ Pages/4_distribution.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.header("Analyse des distributions")
+    st.write("Voyons quelles sont les distributions de tes données, un élément indispenssable pour bien choisir les tests à réaliser")
