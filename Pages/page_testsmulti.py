2cf1cda2106b4a80fffc55dcb78ebcdf6cd076d4
CJ438837 <cedricjadot@msn.com>
2025-11-05 20:51:01 +0100

    Update page_testsmulti.py

--- Pages/page_testsmulti.py
+++ Pages/page_testsmulti.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from modules.IA_STAT_testsmulti import propose_tests_multivariés
+from modules.IA_STAT_testmulti import propose_tests_multivariés
 
 plt.style.use('seaborn-v0_8-muted')
 
