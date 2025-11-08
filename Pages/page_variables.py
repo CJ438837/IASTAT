0c3270470e2ebb5441aabe555b50f0e430063f3d
CJ438837 <cedricjadot@msn.com>
2025-11-08 15:43:39 +0100

    Update button text for navigation to analysis page

--- Pages/page_variables.py
+++ Pages/page_variables.py
@@ -68,6 +68,6 @@ def app():
     st.markdown("---")
     col1, col2, col3 = st.columns([1,2,1])
     with col2:
-        if st.button("➡️ Passer à la page Analyse", use_container_width=True):
+        if st.button("➡️ Passer à la page Analyse Descriptive", use_container_width=True):
             st.session_state.target_page = "Descriptive"
 
