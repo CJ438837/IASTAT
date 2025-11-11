ee16bdb18abaab49b934ab8c84b6869c02f45352
CJ438837 <cedricjadot@msn.com>
2025-11-11 16:26:02 +0100

    Update page_contact.py

--- Pages/page_contact.py
+++ Pages/page_contact.py
@@ -1,1 +1,21 @@
+import streamlit as st
+from PIL import Image
 
+def app():
+    try:
+        logo = Image.open("assets/logo.png")
+        st.image(logo, width=600)
+    except Exception as e:
+        st.warning(f"Logo non trouvé : {e}")
+
+    st.markdown("""
+    **Une question sur l'utilisation, l'interprétation des résultats ou un bug à remonter ?**
+    **N'hésitez pas à me contacter par mail à l'adresse suivante : corvus.analysis@outlook.com ?**
+    """)
+
+    # Bouton de redirection
+    if st.button("📈 Démarrer mon analyse"):
+        st.session_state.target_page = "Fichier"
+
+    st.markdown("---")
+    st.markdown("© 2025 Corvus Analytics - Tous droits réservés")
