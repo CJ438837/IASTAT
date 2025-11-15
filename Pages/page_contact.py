6b47b14190638d524dd109f784154349f2d2b1a3
CJ438837 <cedricjadot@msn.com>
2025-11-15 13:25:04 +0100

    Update page_contact.py

--- Pages/page_contact.py
+++ Pages/page_contact.py
@@ -8,9 +8,5 @@ def app():
     **N'hésitez pas à me contacter par mail à l'adresse suivante : corvus.analysis@outlook.com**
     """)
 
-    # Bouton de redirection
-    if st.button("📈 Démarrer mon analyse"):
-        st.session_state.target_page = "Fichier"
-
     st.markdown("---")
     st.markdown("© 2025 Corvus Analytics - Tous droits réservés")
