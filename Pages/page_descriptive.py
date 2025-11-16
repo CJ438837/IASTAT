1171f09efe65413a646d2ad662bbe1604623d2b9
CJ438837 <cedricjadot@msn.com>
2025-11-16 16:52:45 +0100

    Update page_descriptive.py

--- Pages/page_descriptive.py
+++ Pages/page_descriptive.py
@@ -161,6 +161,10 @@ def app():
         if st.button("➡️ Passer à la page Distribution", use_container_width=True):
             st.session_state.main_page = "Analyse"
             st.session_state.analyse_subpage = "Distribution"
+    with col3:
+        if st.button("➡️ Besoin d'une aide théorique ?", use_container_width=True):
+            st.session_state.main_page = "Théorie"
+            st.session_state.theorie_subpage = "Descriptive"
 
 
 
